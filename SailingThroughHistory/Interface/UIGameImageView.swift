9b43dfc1cc672ca72c9a5240381f6cc6bf0b0b0c
Jason Chong <jasontjakra@gmail.com>
2019-03-18 21:28:22 +0800

    Implement UIGameImageView class

---
+++ SailingThroughHistory/Interface/UIGameImageView.swift
@@ -0,0 +1,22 @@
+//
+//  UIGameImageView.swift
+//  SailingThroughHistory
+//
+//  Created by Jason Chong on 18/3/19.
+//  Copyright © 2019 Sailing Through History Team. All rights reserved.
+//
+
+import UIKit
+
+class UIGameImageView: UIImageView {
+    let object: GameObject
+
+    init(image: UIImage?, object: GameObject) {
+        self.object = object
+        super.init(image: image)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("Decode has not been implemented.")
+    }
+}
