7fbc494770366dd4c9800df054a49d4f44f9552e
Jason Chong <jasontjakra@gmail.com>
2019-03-26 01:16:43 +0800

    Fix interface unit tests

--- SailingThroughHistoryTests/InterfaceTest.swift
+++ SailingThroughHistoryTests/InterfaceTest.swift
@@ -10,9 +10,33 @@ import XCTest
 @testable import SailingThroughHistory
 
 class InterfaceTest: XCTestCase {
-    private let bounds = CGRect.zero
-    private let objectFrame = CGRect.zero
-    private let frame2 = CGRect(origin: CGPoint.zero, size: CGSize(width: 1, height: 1))
+    private var bounds: Rect {
+        guard let frame = Rect(originX: 0, originY: 0,
+                               height: 0, width: 0) else {
+                                fatalError("Invalid bounds")
+        }
+
+        return frame
+    }
+
+    private var objectFrame: Rect {
+        guard let frame = Rect(originX: 0, originY: 0,
+                               height: 0, width: 0) else {
+                                fatalError("Invalid objectFrame")
+        }
+
+        return frame
+    }
+
+    private var frame2: Rect {
+        guard let frame = Rect(originX: 0, originY: 0,
+                               height: 1, width: 1) else {
+                                fatalError("Invalid frame2")
+        }
+
+        return frame
+    }
+
     private lazy var object = GameObject(image: "", frame: objectFrame)
     private lazy var object2 = GameObject(image: "", frame: frame2)
     private lazy var interface = Interface(players: [], bounds: bounds)
@@ -27,27 +51,30 @@ class InterfaceTest: XCTestCase {
     func testAdd() {
         interface.add(object: object)
         interface.broadcastInterfaceChanges(withDuration: 0)
-        XCTAssertEqual(interface.objectFrames[object], objectFrame, "GameObject was not added properly.")
+        XCTAssertEqual(interface.objectFrames.getFrame(for: object), objectFrame, "GameObject was not added properly.")
         interface.add(object: object)
         interface.broadcastInterfaceChanges(withDuration: 0)
-        XCTAssertEqual(interface.objectFrames[object], objectFrame, "GameObject was removed after being added again.")
+        XCTAssertEqual(interface.objectFrames.getFrame(for: object), objectFrame,
+                       "GameObject was removed after being added again.")
 
         interface.add(object: object2)
         interface.broadcastInterfaceChanges(withDuration: 0)
-        XCTAssertEqual(interface.objectFrames[object], objectFrame, "GameObject was removed after being added again.")
-        XCTAssertEqual(interface.objectFrames[object2], frame2, "Second game object" +
+        XCTAssertEqual(interface.objectFrames.getFrame(for: object), objectFrame,
+                       "GameObject was removed after being added again.")
+        XCTAssertEqual(interface.objectFrames.getFrame(for: object2), frame2, "Second game object" +
             " was not added properly.")
 
         object.frame = frame2
         interface.add(object: object)
         interface.broadcastInterfaceChanges(withDuration: 0)
-        XCTAssertEqual(interface.objectFrames[object], frame2, "object frame was not updated after being added again.")
+        XCTAssertEqual(interface.objectFrames.getFrame(for: object), frame2,
+                       "Duplicate object add caused frame to change.")
     }
 
     func testUpdatePosition() {
         interface.updatePosition(of: object)
         interface.broadcastInterfaceChanges(withDuration: 0)
-        XCTAssertNil(interface.objectFrames[object], "Non existant game object was added after move.")
+        XCTAssertNil(interface.objectFrames.getFrame(for: object), "Non existant game object was added after move.")
 
         interface.add(object: object)
         interface.broadcastInterfaceChanges(withDuration: 0)
@@ -55,7 +82,7 @@ class InterfaceTest: XCTestCase {
         object.frame = frame2
         interface.updatePosition(of: object)
         interface.broadcastInterfaceChanges(withDuration: 0)
-        XCTAssertEqual(interface.objectFrames[object], frame2, "Frame was not updated properly.")
+        XCTAssertEqual(interface.objectFrames.getFrame(for: object), frame2, "Frame was not updated properly.")
 
         interface.add(object: object2)
         interface.broadcastInterfaceChanges(withDuration: 0)
@@ -63,8 +90,8 @@ class InterfaceTest: XCTestCase {
         object2.frame = objectFrame
         interface.updatePosition(of: object2)
         interface.broadcastInterfaceChanges(withDuration: 0)
-        XCTAssertEqual(interface.objectFrames[object2], objectFrame, "Frame was not updated properly.")
-        XCTAssertEqual(interface.objectFrames[object], frame2, "Wrong frame was updated")
+        XCTAssertEqual(interface.objectFrames.getFrame(for: object2), objectFrame, "Frame was not updated properly.")
+        XCTAssertEqual(interface.objectFrames.getFrame(for: object), frame2, "Wrong frame was updated")
     }
 
     func testRemove() {
@@ -72,17 +99,17 @@ class InterfaceTest: XCTestCase {
         interface.broadcastInterfaceChanges(withDuration: 0)
         interface.remove(object: object2)
         interface.broadcastInterfaceChanges(withDuration: 0)
-        XCTAssertEqual(interface.objectFrames[object], objectFrame, "Wrong object was affected by remove")
+        XCTAssertEqual(interface.objectFrames.getFrame(for: object), objectFrame, "Wrong object was affected by remove")
 
         interface.add(object: object2)
         interface.broadcastInterfaceChanges(withDuration: 0)
         interface.remove(object: object2)
         interface.broadcastInterfaceChanges(withDuration: 0)
-        XCTAssertNil(interface.objectFrames[object2], "Object not removed properly")
+        XCTAssertNil(interface.objectFrames.getFrame(for: object2), "Object not removed properly")
 
         interface.remove(object: object)
         interface.broadcastInterfaceChanges(withDuration: 0)
-        XCTAssertNil(interface.objectFrames[object], "Object not removed properly")
+        XCTAssertNil(interface.objectFrames.getFrame(for: object), "Object not removed properly")
     }
 
     func testAddRemove() {
@@ -92,7 +119,7 @@ class InterfaceTest: XCTestCase {
         interface.broadcastInterfaceChanges(withDuration: 0)
         interface.add(object: object2)
         interface.broadcastInterfaceChanges(withDuration: 0)
-        XCTAssertEqual(interface.objectFrames[object2], frame2, "Wrong object was affected by remove")
+        XCTAssertEqual(interface.objectFrames.getFrame(for: object2), frame2, "Wrong object was affected by remove")
 
     }
 
