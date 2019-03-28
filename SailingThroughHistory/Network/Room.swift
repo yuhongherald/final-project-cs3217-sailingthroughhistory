870c77e1108e1c698e43c2de89470260134caf2d
Jason Chong <jasontjakra@gmail.com>
2019-03-28 18:23:05 +0800

    Partial implementation for network layer

---
+++ SailingThroughHistory/Network/Room.swift
@@ -0,0 +1,13 @@
+//
+//  Room.swift
+//  SailingThroughHistory
+//
+//  Created by Jason Chong on 27/3/19.
+//  Copyright © 2019 Sailing Through History Team. All rights reserved.
+//
+
+protocol Room {
+    static func getAllRooms(completion: @escaping ([Room]) -> Void)
+    func getConnection(removalCallback: @escaping () -> Void,
+                       completion callback: @escaping (RoomConnection?, Error?) -> ())
+}
