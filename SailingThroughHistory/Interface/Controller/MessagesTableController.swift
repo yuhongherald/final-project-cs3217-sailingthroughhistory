2d1faa0533af4a17349c3a2ea7ab7762b72f3919
Jason Chong <jasontjakra@gmail.com>
2019-04-13 21:42:05 +0800

    General fixes

---
+++ SailingThroughHistory/Interface/Controller/MessagesTableController.swift
@@ -0,0 +1,9 @@
+//
+//  MessagesTableController.swift
+//  SailingThroughHistory
+//
+//  Created by Jason Chong on 13/4/19.
+//  Copyright © 2019 Sailing Through History Team. All rights reserved.
+//
+
+import Foundation
