72549dcd0bed2b2c653047c8add37648894d67e0
henry <yupeihenry@gmail.com>
2019-04-14 05:09:15 -0700

    Delete Consumable, move NPCUI.

--- SailingThroughHistory/Game State/Entities/NPC.swift
+++ SailingThroughHistory/Game State/Entities/NPC.swift
@@ -9,5 +9,15 @@
 import Foundation
 
 class NPC: NodeResident {
-    
+    required override init(nodeId: Int) {
+        super.init(nodeId: nodeId)
+    }
+
+    required init(from decoder: Decoder) throws {
+        try super.init(from: decoder)
+    }
+
+    override func encode(to encoder: Encoder) throws {
+        try super.encode(to: encoder)
+    }
 }
