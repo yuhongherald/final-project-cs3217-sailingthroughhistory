68136a5436db3e3fd48d1145745f487679b5ff84
yuhongherald <yuhongherald@gmail.com>
2019-04-20 06:37:15 -0700

    edits

---
+++ SailingThroughHistoryTests/TurnSystem/EventPresetsTest.swift
@@ -0,0 +1,22 @@
+//
+//  EventPresetsTest.swift
+//  SailingThroughHistoryTests
+//
+//  Created by Herald on 20/4/19.
+//  Copyright © 2019 Sailing Through History Team. All rights reserved.
+//
+
+import XCTest
+@testable import SailingThroughHistory
+
+class EventPresetsTest: XCTestCase {
+    /*
+    func uniqueIdentifierTest() {
+        //let presets = EventPresets()
+
+        var set: [Int: PresetEvent]
+        for preset in presets {
+        }
+    }
+ */
+}
