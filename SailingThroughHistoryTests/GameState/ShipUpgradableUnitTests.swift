6b58270ac41adb56d44d1832a8318120b6a2feef
henry <yupeihenry@gmail.com>
2019-04-20 07:31:26 -0700

    Complete ItemStorage tests.

--- SailingThroughHistoryTests/GameState/ShipUpgradableUnitTests.swift
+++ SailingThroughHistoryTests/GameState/ShipUpgradableUnitTests.swift
@@ -12,5 +12,29 @@ import XCTest
 class ShipUpgradableUnitTests: XCTestCase {
     func testInstallUpgrade() {
         //func installUpgrade(upgrade: Upgrade) -> (Bool, InfoMessage?)
+        /*
+        guard let owner = ship.owner else {
+            return (false, InfoMessage.noOwner)
+        }
+        guard owner.money.value >= upgrade.cost else {
+            return (false, InfoMessage.cannotAfford(upgrade: upgrade))
+        }
+        if ship.shipChassis == nil, let shipUpgrade = upgrade as? ShipChassis {
+            owner.updateMoney(by: -upgrade.cost)
+            ship.shipChassis = shipUpgrade
+            return (true, InfoMessage.upgradePurchased(upgrade: upgrade))
+        }
+        if ship.auxiliaryUpgrade == nil, let auxiliary = upgrade as? AuxiliaryUpgrade {
+            owner.updateMoney(by: -upgrade.cost)
+            ship.auxiliaryUpgrade = auxiliary
+            return (true, InfoMessage.upgradePurchased(upgrade: upgrade))
+        }
+        if upgrade is ShipChassis {
+            return (false, InfoMessage.duplicateUpgrade(type: "Ship Upgrade"))
+        } else if upgrade is AuxiliaryUpgrade {
+            return (false, InfoMessage.duplicateUpgrade(type: "Auxiliary Upgrade"))
+        }
+        return (false, nil)
+        */
     }
 }
