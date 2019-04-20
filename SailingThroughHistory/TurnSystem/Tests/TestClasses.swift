a77ba7fbf6fe9e7642eefc61fec4e3ee4e159b4b
yuhongherald <yuhongherald@gmail.com>
2019-04-20 10:09:15 -0700

    edit

--- SailingThroughHistory/TurnSystem/Tests/TestClasses.swift
+++ SailingThroughHistory/TurnSystem/Tests/TestClasses.swift
@@ -15,6 +15,7 @@ class TestClasses {
         let map = Map(map: "test map", bounds: nil)
         let port = PortStub(buyValueOfAllItems: buyPrice, sellValueOfAllItems: sellPrice)
         map.addNode(port)
+        return map
     }
     static func createLevel() -> GenericLevel {
         return GameParameter(map: createMap(), teams: [team])
@@ -53,63 +54,4 @@ class TestClasses {
 }
 
 class TestBed {
-    func getPresetEventsTest() {
-        let turnSystem = TestClasses.createTestSystem(numPlayers: 0)
-        guard let events = turnSystem.eventPresets?.getEvents() else {
-            //XCTFail("Turn system initialized without event presets")
-            return
-        }
-        let otherEvents = turnSystem.getPresetEvents()
-        //XCTAssertEqual(events.map { $0.displayName }, otherEvents.map { $0.displayName }, "Events not same!")
-    }
-
-    func startGameTest() {
-        let turnSystem0 = TestClasses.createTestSystem(numPlayers: 0)
-        let turnSystem1 = TestClasses.createTestSystem(numPlayers: 1)
-        let turnSystem2 = TestClasses.createTestSystem(numPlayers: 2)
-
-        /*
-        turnSystem0.startGame()
-        XCTAssertEqual(turnSystem0.network.state, TurnSystemNetwork.State.waitForTurnFinish, "Wrong state for 0 players")
-        turnSystem1.startGame()
-        XCTAssertEqual(turnSystem1.network.state,
-                       TurnSystemNetwork.State.waitPlayerInput(
-                       from: turnSystem1.gameState.getPlayers()[0]),
-                       "Wrong state for 1 player")
-         turnSystem2.startGame()
-         XCTAssertEqual(turnSystem2.network.state,
-         TurnSystemNetwork.State.waitPlayerInput(
-         from: turnSystem2.gameState.getPlayers()[0]),
-         "Wrong state for 2 players")
-         */
-    }
-
-    // to represent all the actions
-    func rollTest() {
-        let turnSystem = TestClasses.createTestSystem(numPlayers: 1)
-        turnSystem.startGame()
-        do {
-            _ = try turnSystem.roll(for: turnSystem.gameState.getPlayers()[0])
-        } catch {
-            //XCTFail("Failed to roll the dice, wrong state")
-        }
-    }
-
-    func subscribeToStateTest() {
-        let result = GameVariable<Bool>(value: false)
-        let turnSystem = TestClasses.createTestSystem(numPlayers: 1)
-        turnSystem.subscribeToState {_ in
-            result.value = true
-        }
-        turnSystem.startGame()
-        //XCTAssertTrue(result.value, "Not notified on subscription!")
-    }
-
-    func endTurnTest() {
-        let turnSystem = TestClasses.createTestSystem(numPlayers: 1)
-        turnSystem.startGame()
-        turnSystem.endTurn()
-        //XCTAssertEqual(turnSystem.state, TurnSystemNetwork.State.waitForTurnFinish,
-        //               "Should be waiting for turn to finish!")
-    }
 }
